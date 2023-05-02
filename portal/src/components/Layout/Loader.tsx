aed855e06b33a26be5b39a5781b09c523607c6b2
Bruno Viera <brunovierag@gmail.com>
2023-05-02 10:39:04 -0300

    adding loader component

---
+++ portal/src/components/Layout/Loader.tsx
@@ -0,0 +1,11 @@
+export default function Loader() {
+  return (
+    <div className="loader-wrapper ">
+      <div className="loader">
+        <div></div>
+        <div></div>
+      </div>
+      <p className="loader__text">Cargando ....</p>
+    </div>
+  );
+}
