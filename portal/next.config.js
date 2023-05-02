0c94f1f4ae8c191e70aad978add88853e741a6df
Bruno Viera <brunovierag@gmail.com>
2023-05-02 12:05:54 -0300

    update styles

--- portal/next.config.js
+++ portal/next.config.js
@@ -2,7 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["picsum.photos"],
+    domains: ["picsum.photos", "http2.mlstatic.com"],
+  },
+  compiler: {
+    styledComponents: true,
   },
 };
 
